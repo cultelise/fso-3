dcb7a91e00e51bc7effe99c6af4ab1cab400ee44
elise <115508722+cultelise@users.noreply.github.com>
2022-11-29 16:43:18 -0600

    Added contact db

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
-require('dotenv').config
+require('dotenv').config;
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const Note = require('./models/note')
+const Note = require('./models/note');
+const Contact = require('./models/contact');
+const { response } = require('express');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('build'));
 
-
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2022-05-30T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2022-05-30T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2022-05-30T19:20:14.298Z',
-    important: true,
-  },
-];
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
+// let notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     date: '2022-05-30T17:30:31.098Z',
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only Javascript',
+//     date: '2022-05-30T18:39:34.091Z',
+//     important: false,
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     date: '2022-05-30T19:20:14.298Z',
+//     important: true,
+//   },
+// ];
+// let persons = [
+//   {
+//     id: 1,
+//     name: 'Arto Hellas',
+//     number: '040-123456',
+//   },
+//   {
+//     id: 2,
+//     name: 'Ada Lovelace',
+//     number: '39-44-5323523',
+//   },
+//   {
+//     id: 3,
+//     name: 'Dan Abramov',
+//     number: '12-43-234345',
+//   },
+//   {
+//     id: 4,
+//     name: 'Mary Poppendieck',
+//     number: '39-23-6423122',
+//   },
+// ];
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons);
+  Contact.find({}).then((contact) => {
+    res.json(contact);
+  });
 });
 
 app.get('/info', (req, res) => {
@@ -65,8 +68,9 @@ app.get('/info', (req, res) => {
 });
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
+  Contact.findById(req.params.id).then((contact) => {
+    res.json(contact);
+  });
 
   if (!person) {
     res.status(404).end();
@@ -102,46 +106,53 @@ const getRandomPhone = () => {
 app.post('/api/persons', (req, res) => {
   const body = req.body;
 
-  const person = {
-    id: persons.length + 1,
+  const contact = new Contact({
     name: body.name,
-    number: body.number,
-  };
+    phone: body.phone,
+    date: new Date(),
+    important: true,
+  });
+  contact
+    .save()
+    .then((savedContact) => {
+      console.log(savedContact);
+      console.log(`added ${contact.name} to phonebook.`);
+      res.json(savedContact);
+    })
+    .catch((err) => console.log(err));
 
-  if (persons.find((p) => p.name === person.name)) {
-    return res.status(400).json('Person already exists in phonebook.').end();
-  } else if (persons.find((p) => p.number === person.number)) {
-    return res.status(400).json('Number already exists in phonebook.').end();
-  } else if (!person.number) {
-    return res.status(400).json('Person number missing.');
-  } else if (!person.name) {
-    return res.status(400).json('Person name missing.');
-  } else {
-    persons = persons.concat(person);
-    res.json(person);
-  }
+  // if (persons.find((p) => p.name === person.name)) {
+  //   return res.status(400).json('Person already exists in phonebook.').end();
+  // } else if (persons.find((p) => p.number === person.number)) {
+  //   return res.status(400).json('Number already exists in phonebook.').end();
+  // } else if (!person.number) {
+  //   return res.status(400).json('Person number missing.');
+  // } else if (!person.name) {
+  //   return res.status(400).json('Person name missing.');
+  // } else {
+  //   persons = persons.concat(person);
+  //   res.json(person);
+  // }
 });
 
-app.get('/hello', (request, response) => {
-  response.send('<h1>Hello, Cult!</h1>');
+app.get('/hello', (req, res) => {
+  res.send('<h1>Hello, Cult!</h1>');
 });
 
-app.get('/api/notes', (request, response) => {
+app.get('/api/notes', (req, res) => {
   Note.find({}).then((notes) => {
-    response.json(notes);
+    res.json(notes);
   });
 });
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    Note.find({}).then((notes) => {
-      response.json(notes);
-    });
-  } else {
-    response.status(404).end();
-  }
+app.get('/api/notes/:id', (req, res) => {
+  Note.findById(req.params.id).then((note) => {
+    if (note) {
+        res.json(note);
+      } else {
+      res.status(404).end();
+    }
+  });
 });
 
 const generateId = () => {
@@ -157,16 +168,15 @@ app.post('/api/notes', (req, res) => {
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  res.json(note);
+  note.save().then((savedNote) => {
+    res.json(savedNote);
+  });
 });
 
 app.put('api/notes/:id', (req, res) => {
